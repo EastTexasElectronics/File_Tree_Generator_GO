20aa60e65f935c304cf45bc5b0cd79fbb11c1662
Robert M Havelaar <Contact@EastTexaselectronics.com>
2024-07-29 18:12:33 -0500

    Update

--- ftg.go
+++ ftg.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -9,7 +8,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -17,11 +15,11 @@ import (
 var (
 	excludePatterns = map[string]bool{}
 	outputLocation  string
+	inputDirectory  string
 	version         = "1.0.1"
 	author          = "https://github.com/easttexaselectronics"
 	repository      = "https://github.com/EastTexasElectronics/File-Tree-Generator-Multiverse/tree/main/Go"
 	donation        = "https://www.buymeacoffee.com/rmhavelaar"
-	outputFile      *os.File
 )
 
 func init() {
@@ -29,10 +27,11 @@ func init() {
 }
 
 func showUsage() {
-	fmt.Println(`Usage: ftg [-e pattern1,pattern2,...] [-o output_location] [-i] [-c] [-h] [-v]
+	fmt.Println(`Usage: ftg [-e pattern1,pattern2,...] [-o output_location] [-d input_directory] [-i] [-c] [-h] [-v]
 Options:
   -e, --exclude      Exclude directories or files (comma-separated)(.git,node_modules,.vscode)
   -o, --output       Specify an output location; default output is in the pwd
+  -d, --directory    Specify an input directory; default is the pwd
   -i, --interactive  Interactive mode to select items to exclude
   -c, --clear        Clear the exclusion list
   -h, --help         Show this help message and exit
@@ -41,7 +40,7 @@ Options:
 }
 
 func showVersion() {
-	fmt.Printf("File Tree Generator version: %s\nLeave us a star at %s\n", version, repository)
+	fmt.Printf("File Tree Generator version: %s\nLeave us a star at %s\nAuthor: %s\n", version, repository, author)
 	fmt.Printf("Buy me a coffee: %s\n", donation)
 	os.Exit(0)
 }
@@ -58,32 +57,15 @@ func getEntries(path string) ([]fs.DirEntry, error) {
 	return os.ReadDir(path)
 }
 
-func printEntry(writer io.Writer, name, entryType, indent string, isLast bool) {
-	connector := getConnector(isLast)
-	fmt.Fprintf(writer, "%s%s [%s] %s\n", indent, connector, entryType, name)
-}
-
-func getConnector(isLast bool) string {
+func printEntry(writer io.Writer, name, entryType, prefix string, isLast bool) {
+	var connector string
 	if isLast {
-		return "└──"
+		connector = "└──"
+	} else {
+		connector = "├──"
 	}
-	return "├──"
-}
-
-func processEntry(writer io.Writer, entry fs.DirEntry, path, indent string, isLast bool) {
-	name := entry.Name()
-	fullPath := filepath.Join(path, name)
-	entryType := getEntryType(entry)
-
-	if shouldExclude(name) {
-		return
-	}
-
-	printEntry(writer, name, entryType, indent, isLast)
-
-	if entryType == "Directory" {
-		newIndent := updateIndent(indent, isLast)
-		generateTree(writer, fullPath, newIndent)
+	if _, err := fmt.Fprintf(writer, "%s%s [%s] %s\n", prefix, connector, entryType, name); err != nil {
+		log.Printf("Error writing entry: %v", err)
 	}
 }
 
@@ -94,133 +76,49 @@ func getEntryType(entry fs.DirEntry) string {
 	return "F"
 }
 
-func updateIndent(indent string, isLast bool) string {
-	if isLast {
-		return indent + "    "
-	}
-	return indent + "│   "
-}
-
-func generateTree(writer io.Writer, path, indent string) {
-	entries, err := getEntries(path)
-	if err != nil {
-		errorExit(fmt.Sprintf("Failed to read directory %s: %v", path, err))
-	}
-
+func generateTree(writer io.Writer, path string, prefix string, entries []fs.DirEntry) {
 	for i, entry := range entries {
+		name := entry.Name()
+		if shouldExclude(name) {
+			continue
+		}
+
 		isLast := i == len(entries)-1
-		processEntry(writer, entry, path, indent, isLast)
+		entryType := getEntryType(entry)
+		printEntry(writer, name, entryType, prefix, isLast)
+
+		if entryType == "D" {
+			newPrefix := prefix
+			if isLast {
+				newPrefix += "    "
+			} else {
+				newPrefix += "│   "
+			}
+
+			subEntries, err := getEntries(filepath.Join(path, name))
+			if err != nil {
+				log.Printf("Cannot read directory %s: %v", filepath.Join(path, name), err)
+				continue
+			}
+			generateTree(writer, filepath.Join(path, name), newPrefix, subEntries)
+		}
 	}
 }
 
 func interactiveMode() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		listEntries()
-
-		ids, _ := reader.ReadString('\n')
-		ids = strings.TrimSpace(ids)
-
-		if ids == "clear" {
-			excludePatterns = map[string]bool{}
-			fmt.Println("Exclusion list cleared.")
-		} else {
-			processIDs(ids)
-		}
-
-		showExclusionList()
-
-		fmt.Println("Do you want to add more items (m), generate the file tree (y), or clear the exclusion list (c)?")
-		choice, _ := reader.ReadString('\n')
-		choice = strings.TrimSpace(choice)
-
-		if handleUserChoice(choice) {
-			break
-		}
-	}
-}
-
-func listEntries() {
-	fmt.Println("List of files and directories in the current directory:")
-	entries, err := getEntries(".")
-	if err != nil {
-		errorExit("Failed to read current directory")
-	}
-
-	for i, entry := range entries {
-		fmt.Printf("[%d] %s\n", i+1, entry.Name())
-	}
-}
-
-func processIDs(ids string) {
-	entries, _ := getEntries(".")
-	count := len(entries)
-
-	for _, idStr := range strings.Split(ids, " ") {
-		if idStr == "" {
-			continue
-		}
-		if strings.HasPrefix(idStr, "-") {
-			removeExclusion(entries, count, idStr)
-		} else {
-			addExclusion(entries, count, idStr)
-		}
-	}
-}
-
-func removeExclusion(entries []fs.DirEntry, count int, idStr string) {
-	id, err := strconv.Atoi(idStr[1:])
-	if err == nil && id > 0 && id <= count {
-		entry := entries[id-1]
-		delete(excludePatterns, entry.Name())
-		fmt.Printf("Removed %s from exclusion list.\n", entry.Name())
-	} else {
-		fmt.Printf("Invalid ID: %s\n", idStr)
-	}
-}
-
-func addExclusion(entries []fs.DirEntry, count int, idStr string) {
-	id, err := strconv.Atoi(idStr)
-	if err == nil && id > 0 && id <= count {
-		entry := entries[id-1]
-		excludePatterns[entry.Name()] = true
-		fmt.Printf("Added %s to exclusion list.\n", entry.Name())
-	} else {
-		fmt.Printf("Invalid ID: %s\n", idStr)
-	}
-}
-
-func showExclusionList() {
-	fmt.Println("Current exclusion list:")
-	for pattern := range excludePatterns {
-		fmt.Println(pattern)
-	}
-}
-
-func handleUserChoice(choice string) bool {
-	switch choice {
-	case "y":
-		return true
-	case "c":
-		excludePatterns = map[string]bool{}
-		fmt.Println("Exclusion list cleared.")
-		return false
-	case "m":
-		return false
-	default:
-		fmt.Println("Invalid choice.")
-		return false
-	}
+	fmt.Println("Interactive mode not implemented.")
+	os.Exit(1)
 }
 
 func main() {
 	var exclude string
-	var interactive, clear, help, versionFlag bool
+	var interactive, clearExclusions, help, versionFlag bool
 
 	flag.StringVar(&exclude, "e", "", "Exclude directories or files (comma-separated)")
 	flag.StringVar(&outputLocation, "o", "", "Specify an output location")
+	flag.StringVar(&inputDirectory, "d", "", "Specify an input directory")
 	flag.BoolVar(&interactive, "i", false, "Interactive visual mode to select items to exclude")
-	flag.BoolVar(&clear, "c", false, "Clear the exclusion list")
+	flag.BoolVar(&clearExclusions, "c", false, "Clear the exclusion list")
 	flag.BoolVar(&help, "h", false, "Show this help message and exit")
 	flag.BoolVar(&versionFlag, "v", false, "Show version information and exit")
 
@@ -231,7 +129,7 @@ func main() {
 		showUsage()
 	case versionFlag:
 		showVersion()
-	case clear:
+	case clearExclusions:
 		excludePatterns = map[string]bool{}
 	}
 
@@ -255,22 +153,39 @@ func main() {
 		outputLocation = fmt.Sprintf("file_tree_%s.md", currentTime)
 	}
 
-	currentDir, err := os.Getwd()
-	if err != nil {
-		errorExit("Failed to get current directory")
+	if inputDirectory == "" {
+		var err error
+		inputDirectory, err = os.Getwd()
+		if err != nil {
+			errorExit("Failed to get current directory")
+		}
 	}
 
-	fmt.Printf("Generating your file tree, while you wait... \nGive the project a star at %s\n", repository)
+	fmt.Printf("Generating your file tree for %s, while you wait... \nGive the project a star at %s\n", inputDirectory, repository)
 
-	outputFile, err = os.Create(outputLocation)
+	outputFile, err := os.Create(outputLocation)
 	if err != nil {
 		errorExit(fmt.Sprintf("Cannot write to output location %s", outputLocation))
 	}
-	defer outputFile.Close()
+	defer func() {
+		if err := outputFile.Close(); err != nil {
+			log.Printf("Error closing file: %v", err)
+		}
+	}()
 
-	fmt.Fprintf(outputFile, "# File Tree for %s\n\n## Give the project a star at %s\n```sh\n", currentDir, repository)
-	generateTree(outputFile, ".", "")
-	fmt.Fprintln(outputFile, "```")
+	if _, err := fmt.Fprintf(outputFile, "# File Tree for %s\n\n## Give the project a star at %s\n```sh\n", inputDirectory, repository); err != nil {
+		errorExit(fmt.Sprintf("Error writing to output file: %v", err))
+	}
+
+	entries, err := getEntries(inputDirectory)
+	if err != nil {
+		errorExit("Cannot read the input directory")
+	}
+	generateTree(outputFile, inputDirectory, "", entries)
+
+	if _, err := fmt.Fprintln(outputFile, "```"); err != nil {
+		log.Printf("Error writing to output file: %v", err)
+	}
 
 	fmt.Printf("File tree has been written to %s\n", outputLocation)
 }
